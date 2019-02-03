730679596a4d439990a31010bbc70a489da78794
ecstack <ecstack@sina.com>
2019-02-04 00:44:39 +0800

    测试代码
    完美

--- src/views/hello.vue
+++ src/views/hello.vue
@@ -1,12 +1,76 @@
 <template>
     <div class="hello_container">
       <h1 class="hello">hi </h1>
+      <cube-tab-bar v-model="selectedLabel"
+                    show-slider
+                    :use-transition="disabled"
+                    ref="tabNav"
+                    :data="tabLabels">
+      </cube-tab-bar>
+      <div class="tab-slide-container">
+        <cube-slide
+          ref="slide"
+          :loop="loop"
+          :auto-play="autoPlay"
+          :show-dots="showDots"
+          :options="slideOptions"
+          @scroll="scroll"
+          @change="changePage"
+        >
+          <!-- 关注 -->
+          <cube-slide-item>
+          </cube-slide-item>
+          <!-- 推荐 -->
+          <cube-slide-item>
+          </cube-slide-item>
+          <cube-slide-item>
+          </cube-slide-item>
+        </cube-slide>
+      </div>
     </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
   export default {
-    name: "hello"
+    data () {
+      return {
+        selectedLabel: '推荐',
+        disabled: true,
+        tabLabels: [{
+          label: '关注'
+        }, {
+          label: '推荐'
+        }, {
+          label: '热榜'
+        }],
+        loop: false,
+        autoPlay: false,
+        showDots: false,
+        slideOptions: {
+          listenScroll: true,
+          probeType: 3,
+          /* lock y-direction when scrolling horizontally and  vertically at the same time */
+          directionLockThreshold: 0
+        },
+        scrollOptions: {
+          /* lock x-direction when scrolling horizontally and  vertically at the same time */
+          directionLockThreshold: 0
+        },
+      }
+    },
+    methods: {
+      changePage (current) {
+        this.selectedLabel = this.tabLabels[current].label
+        console.log(current)
+      },
+      scroll (pos) {
+        const x = Math.abs(pos.x)
+        const tabItemWidth = this.$refs.tabNav.$el.clientWidth
+        const slideScrollerWidth = this.$refs.slide.slide.scrollerWidth
+        const deltaX = x / slideScrollerWidth * tabItemWidth
+        this.$refs.tabNav.setSliderTransform(deltaX)
+      },
+    },
   }
 </script>
 
